65ce33525b1509870dff728db7a4aa570c904309
steve <steve@yoyo.cocm>
2020-09-14 10:37:42 +0800

    static files support

--- project_name/asgi.py
+++ project_name/asgi.py
@@ -10,12 +10,21 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+# to support static files
+from asgi_middleware_static_file import ASGIMiddlewareStaticFile
+from django.conf import settings
 from django.core.asgi import get_asgi_application
 
 from websocket.middleware import websockets
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_name.settings')
 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_name.settings')
+
 # application = get_asgi_application()
 application = get_asgi_application()
 application = websockets(application)
+application = ASGIMiddlewareStaticFile(
+    application, static_url=settings.STATIC_URL, static_paths=[settings.STATIC_ROOT]
+)
